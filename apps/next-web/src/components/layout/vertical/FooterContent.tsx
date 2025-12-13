71617601d11a06827599d581ee0701faeeefeb92
Abel Legesse Shiferaw <abel@cheche.et>
2025-12-13 18:25:38 +0300

    feat: add `FooterContent` components for horizontal and vertical layouts.

--- apps/next-web/src/components/layout/vertical/FooterContent.tsx
+++ apps/next-web/src/components/layout/vertical/FooterContent.tsx
@@ -6,16 +6,10 @@ import Link from 'next/link'
 // Third-party Imports
 import classnames from 'classnames'
 
-// Hook Imports
-import useVerticalNav from '@menu/hooks/useVerticalNav'
-
 // Util Imports
 import { verticalLayoutClasses } from '@layouts/utils/layoutClasses'
 
 const FooterContent = () => {
-  // Hooks
-  const { isBreakpointReached } = useVerticalNav()
-
   return (
     <div
       className={classnames(verticalLayoutClasses.footerContent, 'flex items-center justify-between flex-wrap gap-4')}
