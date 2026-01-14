32276093648c2dec206ea42f61d6e6581dcdbd1c
Abenezer Seyoum <abenezerseyoum7@gmail.com>
2026-01-14 09:09:41 +0300

    Use restart instead of reload for Nginx SSL init

--- scripts/init-ssl.sh
+++ scripts/init-ssl.sh
@@ -87,4 +87,5 @@ docker compose -f docker-compose.prod.yml run --rm --entrypoint "\
 echo
 
 echo "### Reloading nginx ..."
-docker compose -f docker-compose.prod.yml exec nginx nginx -s reload
+echo "### Restarting nginx ..."
+docker compose -f docker-compose.prod.yml restart nginx
