563be91572c5f024db4215cf968b1b8e3944981a
Abenezer Seyoum <abenezerseyoum7@gmail.com>
2026-01-15 00:31:43 +0300

    fix ports!

--- apps/express-social/src/index.ts
+++ apps/express-social/src/index.ts
@@ -6,7 +6,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3007;
+const PORT = process.env.PORT || 3003;
 
 app.use(cors());
 app.use(morgan('dev'));
